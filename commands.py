bb9f344d20650c9564a5d1df686f0cca50e3d29c
Maarten van den Berg <maartenberg1@gmail.com>
2017-12-11 18:24:25 +0100

    Partially revert "Some minor fixes in comments"
    This reverts commit 9e22a52122f418b80d076696b182d0451b3362e2.

--- commands.py
+++ commands.py
@@ -96,6 +96,21 @@ def catchall(bot, msg):
     else:
         return 'I do not understand what you want me to do'
 
+def debug_ships() -> [pybattleships.ship.Ship]:
+    ''' Create hardcoded board for the computer to use. '''
+    s1  = Ship.parse_notation('(A1, H, 2)')
+    s2  = Ship.parse_notation('(D1, V, 3)')
+    s3  = Ship.parse_notation('(G1, H, 4)')
+    s4  = Ship.parse_notation('(A3, V, 4)')
+    s5  = Ship.parse_notation('(F3, H, 3)')
+    s6  = Ship.parse_notation('(J3, V, 2)')
+    s7  = Ship.parse_notation('(F6, V, 2)')
+    s8  = Ship.parse_notation('(J6, V, 3)')
+    s9  = Ship.parse_notation('(E10, H, 5)')
+    s10 = Ship.parse_notation('(A8, H, 2)')
+
+    return [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
+
 def to_ledgible(x: int, y:int) -> str:
     a = chr(ord('A') + x)
     b = y+1
