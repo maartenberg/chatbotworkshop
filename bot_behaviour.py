84e5cb2afbeb1cdbff9245113b7e5221146307c0
Robin Sikkens <github@robinsikkens.nl>
2017-12-12 11:24:49 +0100

    MOAR PEP-8

--- bot_behaviour.py
+++ bot_behaviour.py
@@ -8,19 +8,23 @@ import pybattleships.game
 
 def initialize(bot):
     '''
-    This function will be called before any others, use this to define a starting state or taunt the player
+    This function will be called before any others,
+    use this to define a starting state or taunt the player
     by typing:
     bot.sender.sendMessage("*YOUR AWESOME TAUNT*")
     somewhere before return
     '''
-    bot.fired_at_list = [] # This list keeps track of the squares we have already shot at
-    bot.results = []       # A list to keep track of shot results, form of (x, y, ShotResult)
+    # Make a list that keeps track of the squares we have already shot at
+    bot.fired_at_list = []
+    # Make a list to keep track of shot results, form of (x, y, ShotResult)
+    bot.results = []
     return
 
 def generate_hit(bot) -> (int, int):
     '''
     This function will be called any time your bot has to decide what to do.
-    Remeber that a tuple of (x, y) values are expected and both x and y have to be an integer from 0-9
+    Remeber that a tuple of (x, y) values are expected and both x and y have
+    to be an integer in the range 0-9
     '''
     x = random.randrange(10)
     y = random.randrange(10)
@@ -36,12 +40,14 @@ def generate_hit(bot) -> (int, int):
 def generate_board(bot) -> pybattleships.board.Board:
     '''
     This function will be called when it's the bot's turn to set up a board.
-    You can keep it as is but that would be pretty boring to play against, so it;s best to come up with a way to
-    generate a board automatically.
+    You can keep it as is but that would be pretty boring to play against,
+    so it's best to come up with a way to generate a board automatically.
     '''
     s1  = Ship.parse_notation('(A1, H, 2)')
-    s2  = Ship(3, 0, False, 3) #Another (easier) way to make a ship
-                               # this is the same ship as Ship.parse_notation('(D1, V, 3)') would have made.
+    # You can also use another (easier) way to make a ship
+    s2  = Ship(3, 0, False, 3)
+    # this is the same ship as
+    # Ship.parse_notation('(D1, V, 3)') would have made.
     s3  = Ship.parse_notation('(G1, H, 4)')
     s4  = Ship.parse_notation('(A3, V, 4)')
     s5  = Ship.parse_notation('(F3, H, 3)')
